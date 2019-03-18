bc086191a76a00c14cd501313cd11122b578b9b6
Shakawat Hossain <shakawat176464@gmail.com>
2019-03-19 03:31:04 +0600

    third Commit

--- app/src/main/java/me/h/exclusive/myapplication/MainActivity.java
+++ app/src/main/java/me/h/exclusive/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package me.h.exclusive.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
